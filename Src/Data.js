d59e18c0bdaca724ad80014e41b04eedc60773be
evaanbailleul59 <evaanbailleul@gmail.com>
2025-08-24 02:48:05 +0200

    Create Data.js

---
+++ Src/Data.js
@@ -0,0 +1,72 @@
+export const produits = [
+  // Softs
+  {id:1,nom:"Coca 33cl",prix:1.2,categorie:"Softs"},
+  {id:2,nom:"Oasis 33cl",prix:1.2,categorie:"Softs"},
+  {id:3,nom:"Thé 33cl",prix:1.2,categorie:"Softs"},
+  {id:4,nom:"Eau 50cl",prix:0.5,categorie:"Softs"},
+  {id:5,nom:"Eau 1L",prix:1.0,categorie:"Softs"},
+  {id:6,nom:"Bouteille Soft 50cl",prix:1.9,categorie:"Softs"},
+  
+  // Énergisants
+  {id:7,nom:"Monster 50cl",prix:2.5,categorie:"Énergisants"},
+  {id:8,nom:"RedBull 33cl",prix:2.5,categorie:"Énergisants"},
+  {id:9,nom:"RedBull goût 33cl",prix:2.5,categorie:"Énergisants"},
+  {id:10,nom:"Powerade Bleu",prix:2.5,categorie:"Énergisants"},
+  {id:11,nom:"Powerade Rouge",prix:2.5,categorie:"Énergisants"},
+  
+  // Sandwichs
+  {id:12,nom:"Sandwich Poulet Sodebo",prix:3.9,categorie:"Sandwichs"},
+  {id:13,nom:"Sandwich Thon Mayo Sodebo",prix:3.9,categorie:"Sandwichs"},
+  {id:14,nom:"Sandwich Œuf Mayo Sodebo",prix:3.9,categorie:"Sandwichs"},
+  
+  // Snacks
+  {id:15,nom:"Chips Brets",prix:2.5,categorie:"Snacks"},
+  {id:16,nom:"Lays",prix:2.9,categorie:"Snacks"},
+  {id:17,nom:"LU Prince",prix:2.1,categorie:"Snacks"},
+  {id:18,nom:"Bonbons 100g",prix:2.0,categorie:"Snacks"},
+  {id:19,nom:"Pâtes 500g",prix:1.5,categorie:"Snacks"},
+  {id:20,nom:"Crème fraîche",prix:2.9,categorie:"Snacks"},
+  
+  // Alcool Vodka
+  {id:21,nom:"Poliakov 20cl",prix:5.9,categorie:"Alcool"},
+  {id:22,nom:"Poliakov 35cl",prix:11.9,categorie:"Alcool"},
+  {id:23,nom:"Poliakov 70cl",prix:17.9,categorie:"Alcool"},
+  {id:24,nom:"Poliakov 1L",prix:26.9,categorie:"Alcool"},
+  {id:25,nom:"Sobieski 20cl",prix:6.9,categorie:"Alcool"},
+  {id:26,nom:"Sobieski 35cl",prix:12.9,categorie:"Alcool"},
+  {id:27,nom:"Sobieski 70cl",prix:19.9,categorie:"Alcool"},
+  {id:28,nom:"Sobieski 1L",prix:27.9,categorie:"Alcool"},
+  {id:29,nom:"Eristoff 20cl",prix:4.9,categorie:"Alcool"},
+  {id:30,nom:"Eristoff 35cl",prix:10.9,categorie:"Alcool"},
+  {id:31,nom:"Eristoff 70cl",prix:16.9,categorie:"Alcool"},
+  {id:32,nom:"Eristoff 1L",prix:25.9,categorie:"Alcool"},
+  {id:33,nom:"Eristoff 1.5L",prix:37.9,categorie:"Alcool"},
+  {id:34,nom:"Absolut 20cl",prix:9.9,categorie:"Alcool"},
+  {id:35,nom:"Absolut 35cl",prix:14.9,categorie:"Alcool"},
+  {id:36,nom:"Absolut 70cl",prix:25.9,categorie:"Alcool"},
+  {id:37,nom:"Absolut 1L",prix:34.9,categorie:"Alcool"},
+  {id:38,nom:"Ciroc 70cl",prix:37.0,categorie:"Alcool"},
+  {id:39,nom:"Belvédère 20cl",prix:20.9,categorie:"Alcool"},
+  {id:40,nom:"Belvédère 70cl",prix:46.9,categorie:"Alcool"},
+  {id:41,nom:"Grey Goose 70cl",prix:49.9,categorie:"Alcool"},
+  
+  // Whisky
+  {id:42,nom:"William Peel 20cl",prix:6.9,categorie:"Alcool"},
+  {id:43,nom:"William Peel 35cl",prix:12.9,categorie:"Alcool"},
+  {id:44,nom:"William Peel 70cl",prix:18.9,categorie:"Alcool"},
+  {id:45,nom:"William Peel 1L",prix:27.9,categorie:"Alcool"},
+  {id:46,nom:"William Peel 1.5L",prix:37.9,categorie:"Alcool"},
+  {id:47,nom:"Ballantines 20cl",prix:10.9,categorie:"Alcool"},
+  {id:48,nom:"Ballantines 35cl",prix:16.9,categorie:"Alcool"},
+  {id:49,nom:"Ballantines 70cl",prix:24.9,categorie:"Alcool"},
+  {id:50,nom:"Ballantines 1L",prix:34.9,categorie:"Alcool"},
+  {id:51,nom:"Ballantines 1.5L",prix:49.9,categorie:"Alcool"},
+  {id:52,nom:"Jack Daniel's 20cl",prix:12.9,categorie:"Alcool"},
+  {id:53,nom:"Jack Daniel's 35cl",prix:19.9,categorie:"Alcool"},
+  {id:54,nom:"Jack Daniel's 70cl",prix:31.9,categorie:"Alcool"},
+  {id:55,nom:"Jack Daniel's 1L",prix:44.9,categorie:"Alcool"},
+  {id:56,nom:"Jack Daniel's 1L Miel",prix:45.9,categorie:"Alcool"},
+  
+  // Flash Ciroc
+  {id:57,nom:"Flash Ciroc (RedBull + Ciroc 20cl)",prix:13.0,categorie:"Alcool"}
+];
